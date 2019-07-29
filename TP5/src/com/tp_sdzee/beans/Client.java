ff516adb90a552b35825537a7c96760ab1de864f
stagiaire <stagiaire@VM-Ubuntu>
2019-07-29 15:25:03 +0200

    Commit TP5

---
+++ TP5/src/com/tp_sdzee/beans/Client.java
@@ -0,0 +1,60 @@
+package com.tp_sdzee.beans;
+
+public class Client {
+
+    private String nomClient;
+    private String prenomClient;
+    private String adresseClient;
+    private String telephoneClient;
+    private String emailClient;
+    private String nomImage;
+
+    public String getNomClient() {
+        return nomClient;
+    }
+
+    public void setNomClient( String nomClient ) {
+        this.nomClient = nomClient;
+    }
+
+    public String getPrenomClient() {
+        return prenomClient;
+    }
+
+    public void setPrenomClient( String prenomClient ) {
+        this.prenomClient = prenomClient;
+    }
+
+    public String getAdresseClient() {
+        return adresseClient;
+    }
+
+    public void setAdresseClient( String adresseClient ) {
+        this.adresseClient = adresseClient;
+    }
+
+    public String getTelephoneClient() {
+        return telephoneClient;
+    }
+
+    public void setTelephoneClient( String telephoneClient ) {
+        this.telephoneClient = telephoneClient;
+    }
+
+    public String getEmailClient() {
+        return emailClient;
+    }
+
+    public void setEmailClient( String emailClient ) {
+        this.emailClient = emailClient;
+    }
+
+    public String getNomImage() {
+        return nomImage;
+    }
+
+    public void setNomImage( String nomImage ) {
+        this.nomImage = nomImage;
+    }
+
+}
